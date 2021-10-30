507161e2eddfc83675b68cc3f971720d27517cdf
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-10-30 17:12:16 -0300

    :rocket:nova feature - config sequelize the project

---
+++ auth-api/src/config/db/dbConfig.js
@@ -0,0 +1,31 @@
+import Sequelize from "sequelize";
+
+
+const sequelize = new Sequelize("auth-db", "postgres", "admin", {
+    host: "localhost",
+    dialect: "postgres",
+    quoteIdentifiers: false,
+    define: {
+        syncOnAssociation: true,
+        timestamps: false,
+        underscored: true,
+        underscoredAll: true,
+        freezeTableName: true,
+      },
+      pool: {
+        acquire: 180000,
+      },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.info("Connection has been stablished!");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database.");
+    console.error(err.message);
+  });
+
+
+export default sequelize;
