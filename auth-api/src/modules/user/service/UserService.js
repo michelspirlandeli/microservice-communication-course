898736c3b11f60de596ed5d5d821bbb967b1d66c
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-09 10:50:09 -0300

    :rocket:nova feature - create auth witch JWT

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,5 +1,10 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import UserRepository from "../repository/UserRepository.js";
+import UserException from "../exception/UserException.js";
 import * as httpStatus from "../../../config/constants/httpStatus.js";
+import * as secrets from "../../../config/constants/secrets.js";
 class UserService { 
 
     async findByEmail(req) {
@@ -33,6 +38,50 @@ class UserService {
       validateUserNotFound(user) {
         if (!user) {
           throw new UserException(httpStatus.BAD_REQUEST, "User was not found.");
+        }
+      }
+
+      async getAccessToken(req) {
+        try {
+          const { email, password } = req.body;
+          this.validateAccessTokenData(email, password);
+          let user = await UserRepository.findByEmail(email);
+          this.validateUserNotFound(user);
+          await this.validatePassword(password, user.password);
+          const authUser = { id: user.id, name: user.name, email: user.email };
+          const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, {
+            expiresIn: "1d",
+          });
+    
+          let response = {
+            status: httpStatus.SUCCESS,
+            accessToken,
+          };
+         
+          return response;
+        } catch (err) {
+          return {
+            status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+            message: err.message,
+          };
+        }
+      }
+
+      validateAccessTokenData(email, password) {
+        if (!email || !password) {
+          throw new UserException(
+            httpStatus.UNAUTHORIZED,
+            "Email and password must be informed."
+          );
+        }
+      }
+    
+      async validatePassword(password, hashPassword) {
+        if (!(await bcrypt.compare(password, hashPassword))) {
+          throw new UserException(
+            httpStatus.UNAUTHORIZED,
+            "Password doesn't match."
+          );
         }
       }
 }
