3374825d8da23cae403f0c5b2916700e0e708913
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-11-09 14:19:02 -0300

    :rocket:nova feature - add middleware for authentication

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,14 +1,20 @@
-import express  from "express";
+import express from "express";
+
 import * as db from "./src/config/db/initialData.js";
 import userRoutes from "./src/modules/user/routes/UserRoutes.js";
+import checkToken from "./src/config/auth/checkToken.js";
 
 const app = express();
 const env = process.env;
-const PORT =  env.PORT || 8090;
+const PORT = env.PORT || 8090;
 
+db.createInitialData();
 
+app.use(express.json());
 
+app.use(userRoutes);
 
+app.use(checkToken);
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
@@ -17,10 +23,6 @@ app.get('/api/status', (req, res) => {
         httpStatus: 200
     });
 });
-
-app.use(express.json());
-db.createInitialData();
-app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started sucessfully at port ${PORT}`);
